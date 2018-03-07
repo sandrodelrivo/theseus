5df4cdd9b4c172f6c03c84d7dd797d43a3387c4e
Sandro <sandro.delrivo@gmail.com>
2018-03-06 22:14:38 -0800

    Initial Commit
    Add basic cave map

---
+++ caves/simple_dungeon_set.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="simple_set" tilewidth="16" tileheight="16" tilecount="960" columns="30">
+ <image source="../../dungeon tileset calciumtrice simple.png" width="480" height="512"/>
+ <tile id="30">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="31">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="32">
+  <objectgroup draworder="index">
+   <object id="3" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="2" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index">
+   <object id="2" x="4.25" y="1.375" width="4.5" height="11.625"/>
+   <object id="5" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index">
+   <object id="2" x="6.875" y="4.75" height="0.125"/>
+   <object id="4" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="62">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="63">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="-0.125" width="0.125" height="0.125"/>
+   <object id="2" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="64">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="65">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index">
+   <object id="1" type="collision" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ caves/test_dungeon.tsx
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dungeon tileset calciumtrice" tilewidth="16" tileheight="16" tilecount="1050" columns="30">
+ <image source="../../dungeon tileset calciumtrice.png" width="480" height="560"/>
+ <tile id="120">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index">
+   <object id="1" name="col" type="area" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="122">
+  <objectgroup draworder="index">
+   <object id="1" name="col" type="StaticBody2d" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="158">
+  <objectgroup draworder="index">
+   <object id="1" x="-1" y="5"/>
+   <object id="2" x="14" y="4"/>
+   <object id="3" x="1" y="5"/>
+  </objectgroup>
+ </tile>
+</tileset>
