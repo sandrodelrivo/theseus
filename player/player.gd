4a6f7c59706b30beb1f25db2a4b7d133fdc23860
HColeman127 <harrymcoleman@gmail.com>
2018-03-10 15:55:01 -0800

    Script Cleanup
    general cleanup of scripts
    
    add ui scene instance with fps counter

--- player/player.gd
+++ player/player.gd
@@ -1,60 +1,47 @@
 extends KinematicBody2D
 
-# Runs when player node is loaded
-func _ready():
-	print("Player Script Ready!")
-	set_physics_process(true)
-	#set_process_input(true)
-
-# Constants
-var SPEED = 50
-
-# Member Variables
+# variables
+var speed = 50
 var velocity = Vector2()
 var facing = Vector2()
 
-# Runs every physics tick
+# runs when node is loaded
+func _ready():
+	print("Player Script Ready!")
+	#set_physics_process(false)
+	#set_process_input(true)
+
+# runs every physics tick
 func _physics_process(delta):
 	
-	setFacing()
-	$sprite.frame = getDirection(facing)
+	# mouse position vector minus player postion vector
+	facing = get_viewport().get_mouse_position() - get_global_transform_with_canvas()[2]
+	# converts angle to int on interval [0,7] with 0rad=0 increasing clockwise
+	$sprite.frame = range(8)[int(round(facing.angle()/(PI/4)))]
 	
+	# sets velocity based on what keys are currently pressed and moves
 	setVelocity()
 	move_and_slide(velocity)
+	
+	print("player", velocity)
 
 
-# Extra Functions
+# extra functions
 
-# Checks what controls are pressed and sets velocity
+# checks what controls are pressed and sets velocity
 func setVelocity():
+	# sets to 0
+	velocity = Vector2(0,0)
+	
+	# adds 1 to either horizonta or vertical depending on keypress
 	if (Input.is_action_pressed("ui_up")):
-		velocity.y = -1
-	elif (Input.is_action_pressed("ui_down")):
-		velocity.y = 1
-	else:
-		velocity.y = 0
-	
+		velocity.y-=1
+	if (Input.is_action_pressed("ui_down")):
+		velocity.y+=1
 	if (Input.is_action_pressed("ui_right")):
-		velocity.x = 1
-	elif (Input.is_action_pressed("ui_left")):
-		velocity.x = -1
-	else:
-		velocity.x = 0
+		velocity.x+=1
+	if (Input.is_action_pressed("ui_left")):
+		velocity.x-=1
 	
-	#Sets magnitude of velocity to constant SPEED
-	velocity = velocity.normalized()*SPEED
-
-# Finds the vector between the screen positon of the player to
-# 	the screen postion of the mouse 
-func setFacing():
-	var pos_player = get_global_transform_with_canvas()[2]
-	var pos_mouse = get_viewport().get_mouse_position()
-	
-	facing = pos_mouse - pos_player
-
-func getDirection(vector):
-	var raw = int(round(vector.angle()/(PI/4)))
-	if (raw<0):
-		raw += 8
-	
-	return raw
+	# sets magnitude of velocity to speed
+	velocity = velocity.normalized()*speed
