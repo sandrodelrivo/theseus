5e059d399193c02f3328df5dcbb062b8d8206d64
HColeman127 <harrymcoleman@gmail.com>
2018-03-09 17:08:07 -0800

    Filtree Reformat
    reorganize filetree
    
    player now faces towards mouse

--- player/player.gd
+++ player/player.gd
@@ -1,29 +1,31 @@
 extends KinematicBody2D
 
+# Runs when player node is loaded
 func _ready():
 	print("Player Script Ready!")
 	set_physics_process(true)
 	#set_process_input(true)
 
+# Constants
 var SPEED = 50
 
+# Member Variables
 var velocity = Vector2()
 var facing = Vector2()
 
-# note - rotation is in radians, not degrees
+# Runs every physics tick
 func _physics_process(delta):
 	
-	setVelocity()
 	setFacing()
-		
-	if (facing.x!=0 or facing.y!=0):
-		setFrame(getDirection(facing))
-		
-	move_and_slide(velocity.normalized()*SPEED)
-
+	$sprite.frame = getDirection(facing)
 	
+	setVelocity()
+	move_and_slide(velocity)
+
+
 # Extra Functions
 
+# Checks what controls are pressed and sets velocity
 func setVelocity():
 	if (Input.is_action_pressed("ui_up")):
 		velocity.y = -1
@@ -31,34 +33,28 @@ func setVelocity():
 		velocity.y = 1
 	else:
 		velocity.y = 0
-		
+	
 	if (Input.is_action_pressed("ui_right")):
 		velocity.x = 1
 	elif (Input.is_action_pressed("ui_left")):
 		velocity.x = -1
 	else:
 		velocity.x = 0
+	
+	#Sets magnitude of velocity to constant SPEED
+	velocity = velocity.normalized()*SPEED
 
+# Finds the vector between the screen positon of the player to
+# 	the screen postion of the mouse 
 func setFacing():
-	if (Input.is_action_pressed("face_up")):
-		facing.y = -1
-	elif (Input.is_action_pressed("face_down")):
-		facing.y = 1
-	else:
-		facing.y = 0
-		
-	if (Input.is_action_pressed("face_right")):
-		facing.x = 1
-	elif (Input.is_action_pressed("face_left")):
-		facing.x = -1
-	else:
-		facing.x = 0
+	var pos_player = get_global_transform_with_canvas()[2]
+	var pos_mouse = get_viewport().get_mouse_position()
+	
+	facing = pos_mouse - pos_player
 
 func getDirection(vector):
-	var raw = int(vector.angle()/(PI/4))
+	var raw = int(round(vector.angle()/(PI/4)))
 	if (raw<0):
-		return raw+8
-	return raw
+		raw += 8
 	
-func setFrame(direction):
-	$sprite.frame = direction
+	return raw
