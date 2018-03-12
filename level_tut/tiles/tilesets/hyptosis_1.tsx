4e93d269583ef9fa49a4490555715be97c6c248c
Sandro <sandro.delrivo@gmail.com>
2018-03-11 18:06:47 -0700

    Add Player FNS
    Add finite state machine setup for player input
    Change tilemap to new test map

---
+++ level_tut/tiles/tilesets/hyptosis_1.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="hyptosis_1" tilewidth="32" tileheight="32" tilecount="900" columns="30">
+ <image source="../assets/tiles/hyptosis_tile-art-batch-1.png" width="960" height="960"/>
+ <tile id="782">
+  <objectgroup draworder="index">
+   <object id="2" name="collision" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
--- player/player.gd
+++ player/player.gd
@@ -1,13 +1,28 @@
 extends KinematicBody2D
 
 # variables
-var speed = 50
+var speed_max = 100
+var isMoving = false
+var speed = 0
+var dashSpeed = 5000
+var acc = 10
+var dec = 20
+var accelearation
 var velocity = Vector2()
 var facing = Vector2()
+var direction = 0
+onready var input
+
+enum STATE{
+	idle,
+	move,
+	dash	
+}
 
 # runs when node is loaded
 func _ready():
 	print("Player Script Ready!")
+	STATE = idle
 	#set_physics_process(false)
 	#set_process_input(true)
 
@@ -20,35 +35,102 @@ func _physics_process(delta):
 	$sprite.frame = range(8)[int(round(facing.angle()/(PI/4)))]
 	
 	# sets velocity based on what keys are currently pressed and moves
-	setVelocity()
-	move_and_slide(velocity)
+
+	print(STATE)
+	match STATE:
+		idle:
+			# determine next state (check transitions)
+			if checkMovement():
+				STATE = move
+			elif checkDash():
+				STATE = dash
+			else:
+				# deccelerate the player		
+				if speed > 0:
+					speed -= dec
+			
+		move:
+			setVelocity()
+			move_and_slide(velocity)
+			
+			# determine next state (check transitions)
+			if not checkMovement():
+				STATE = idle
+			elif checkDash():
+				STATE = dash
+					
+		dash:
+			dash()
+			
+			# determine next state (check transitions)
+			if checkMovement():
+				STATE = move
+			else: 
+				STATE = idle
 
 
 # extra functions
+
+func checkMovement():
+	var movement = false
+	if (Input.is_action_pressed("ui_up")):
+		movement = true
+	if (Input.is_action_pressed("ui_down")):
+		movement = true
+	if (Input.is_action_pressed("ui_right")):
+		movement = true
+	if (Input.is_action_pressed("ui_left")):
+		movement = true
+		
+	return movement
+	
+func checkDash():
+	if (Input.is_action_just_pressed("click_right")):
+		return true
+	else:
+		return false
+	
 
 # checks what controls are pressed and sets velocity
 func setVelocity():
 	# sets to 0
 	velocity = Vector2(0,0)
 	
-	# adds 1 to either horizonta or vertical depending on keypress
+	# adds 1 to either horizontal or vertical depending on keypress
+	
 	if (Input.is_action_pressed("ui_up")):
 		velocity.y-=1
+		if speed < speed_max:
+			speed += acc
 	if (Input.is_action_pressed("ui_down")):
 		velocity.y+=1
+		if speed < speed_max:
+			speed += acc
 	if (Input.is_action_pressed("ui_right")):
 		velocity.x+=1
+		if speed < speed_max:
+			speed += acc
 	if (Input.is_action_pressed("ui_left")):
 		velocity.x-=1
-	
+		if speed < speed_max:
+			speed += acc
+
+		
 	# sets magnitude of velocity to speed
 	velocity = velocity.normalized()*speed
+
+
 	
-func _input(event):
-	if event.is_action_pressed("click_left") and event.is_pressed():
-		# sets tele_test position ot mouse position
-		var check = get_parent().get_node("tele_test")
-		check.position = get_global_mouse_position()
-		if ():
-			position = check
+func dash():
+	
+	velocity.x = dashSpeed * cos(facing.angle())
+	velocity.y = dashSpeed * sin(facing.angle())	
+	
+	var motion = velocity
+	motion = move_and_slide(motion)	
+	
+	STATE = idle
+	
+
+		
 		
