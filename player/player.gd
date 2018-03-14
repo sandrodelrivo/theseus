98635b08840ce756e1da289422abd483900eefd0
HColeman127 <harrymcoleman@gmail.com>
2018-03-13 18:56:31 -0700

    State Machine Rework
    add new functions for handling state switching
    add labels for current state and current speed

--- player/player.gd
+++ player/player.gd
@@ -1,22 +1,22 @@
 extends KinematicBody2D
 
+
 # variables
-var speed_max = 100
-var isMoving = false
-var speed = 0
-var dashSpeed = 4000 # how far the dash moves
+#  movement
+var speedMax = 100 # upper limit of speedCurrent
+var speedCurrent = 0 # magnitude to which velocity is set
+var speedDash = 100 # how far the dash moves
 var acc = 10 # acceleration interval
 var dec = 20 # deceleration interval
-var accelearation
-var velocity = Vector2()
+var movement = Vector2() # vector which determines movement each physics tick
+#  facing
 var facing = Vector2()
 var direction = 0
-onready var input
 
 enum STATE{
 	idle,
 	move,
-	dash	
+	dash
 }
 
 enum COMBAT_STATE{
@@ -38,102 +38,105 @@ func _physics_process(delta):
 	# converts angle to int on interval [0,7] with 0rad=0 increasing clockwise
 	$sprite.frame = range(8)[int(round(facing.angle()/(PI/4)))]
 	
-	# sets velocity based on what keys are currently pressed and moves
-
-	print(STATE)
+	# performs state processes which need to run each physics tick
 	match STATE:
 		idle:
-			# determine next state (check transitions)
-			if checkMovement():
-				STATE = move
-			elif checkDash():
-				STATE = dash
-			else:
-				# deccelerate the player		
-				if speed > 0:
-					speed -= dec
-			
+			pass
 		move:
-			setVelocity()
-			move_and_slide(velocity)
-			
-			# determine next state (check transitions)
-			if not checkMovement():
-				STATE = idle
-			elif checkDash():
-				STATE = dash
-					
+			movement = get_velocity()
+			move_and_slide(movement)
 		dash:
-			dash()
-			
+			pass
+
+# runs when an input event occurs and determines state change
+func _input(event):
+	var event_list = getActions()
+	match STATE:
+		idle:
+			speedCurrent = 0
 			# determine next state (check transitions)
-			if checkMovement():
+			if event_list["move"]:
 				STATE = move
+			if event_list["dash"]:
+				dash()
+		move:
+			# determine next state (check transitions)
+			if event_list["dash"]:
+				dash()
+			elif not event_list["move"]:
+				setState("idle")
+		dash:
+			# determine next state (check transitions)
+			if event_list["move"]:
+				setState("move")
 			else: 
-				STATE = idle
+				setState("idle")
+	#rint(event, STATE)
 
-
-# extra functions
-
-func checkMovement():
-	var movement = false
-	if (Input.is_action_pressed("ui_up")):
-		movement = true
-	if (Input.is_action_pressed("ui_down")):
-		movement = true
-	if (Input.is_action_pressed("ui_right")):
-		movement = true
-	if (Input.is_action_pressed("ui_left")):
-		movement = true
-		
-	return movement
+# runs when state is changed, performs exit and enter processes
+func setState(new_state):
+	# perform exit state processes
+	match (STATE):
+		"idle":
+			pass
+		"move":
+			pass
+		"dash":
+			pass
 	
-func checkDash():
+	# perform enter state processes
+	match (new_state):
+		"idle": 
+			STATE = idle
+			speedCurrent = 0
+		"move":
+			STATE = move
+		"dash":
+			STATE = dash
+
+# looks at input and returns a dictionary of input states
+func getActions():
+	var event_list = {"move":false, "dash":false}
+	
+	# checks if any movement keys are held --> movement
+	if (Input.is_action_pressed("ui_up") or 
+		Input.is_action_pressed("ui_down") or 
+		Input.is_action_pressed("ui_right") or 
+		Input.is_action_pressed("ui_left")):
+		event_list["move"] = true
+	
+	# checks if the right mouse has just been pressed --> dash
 	if (Input.is_action_just_pressed("click_right")):
-		return true
-	else:
-		return false
+		event_list["dash"] = true
+		
+	return event_list
 	
 
 # checks what controls are pressed and sets velocity
-func setVelocity():
-	# sets to 0
-	velocity = Vector2(0,0)
+func get_velocity():
+	# sets velocity to 0
+	var velocity = Vector2(0,0)
 	
 	# adds 1 to either horizontal or vertical depending on keypress
-	
 	if (Input.is_action_pressed("ui_up")):
 		velocity.y-=1
-		if speed < speed_max:
-			speed += acc
 	if (Input.is_action_pressed("ui_down")):
 		velocity.y+=1
-		if speed < speed_max:
-			speed += acc
 	if (Input.is_action_pressed("ui_right")):
 		velocity.x+=1
-		if speed < speed_max:
-			speed += acc
 	if (Input.is_action_pressed("ui_left")):
 		velocity.x-=1
-		if speed < speed_max:
-			speed += acc
-
-		
-	# sets magnitude of velocity to speed
-	velocity = velocity.normalized()*speed
-
+	
+	# increases speed by acc if it is less than speedMax
+	if speedCurrent < speedMax:
+			speedCurrent += acc
+	
+	# return velocity with magnitude of the current logical speed
+	return velocity.normalized()*speedCurrent
 	
 func dash():
+	var dash = get_global_mouse_position()-position
+	if (dash.length()>speedDash):
+		dash = dash.normalized()*speedDash
 	
-	velocity.x = dashSpeed * cos(facing.angle())
-	velocity.y = dashSpeed * sin(facing.angle())	
-	
-	var motion = velocity
-	motion = move_and_slide(motion)	
-	
-	STATE = idle
-	
-
-		
-		
+	move_and_slide(dash*60)
