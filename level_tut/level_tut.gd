522820fd856aeb737272b61035d13559f544995f
HColeman127 <harrymcoleman@gmail.com>
2018-03-15 16:56:43 -0700

    Enemy Spawners
    add handling for placeholder enemy nodes
    add spawner for bat

--- level_tut/level_tut.gd
+++ level_tut/level_tut.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+
+
 # onready's
 
 
@@ -8,19 +10,47 @@ extends Node2D
 onready var player = $player
 #  enemies
 onready var enemy_spawners = $enemies.get_children()
-var enemy_instances = []
+var enemy_instances = {}
+
+# temp
+var pos_player
+var pos_spawner
+var pos_instance
 
 func _ready():
 	print("Main Script Ready!")
 	
-	spawn_enemies()
-	
-
-func spawn_enemies():
-	for i in enemy_spawners:
-		var e = i.to_spawn.instance()
-		$enemies.add_child(e)
-		e.position = i.position
-		i.visible = false
-		enemy_instances.append(e)
+func _physics_process(delta):
+	for spawner in enemy_spawners:
+		var player_to_spawner = spawner.position.distance_to(player.position)
 		
+		if (spawner.visible and 50<player_to_spawner and player_to_spawner<100):
+			spawn(spawner)
+		elif ((not spawner.visible) and 100<enemy_instances[spawner].position.distance_to(player.position)):
+			despawn(spawner)
+			
+	
+func _input(event):
+	if Input.is_action_just_pressed("face_up"):
+		spawn(enemy_spawners[0])
+
+
+func spawn(spawner):
+	# create new dictionary entry with key of spawner and key value of an instance of the enemy scene
+	enemy_instances[spawner] = spawner.to_spawn.instance()
+	# adds the instance to the enemy container
+	$enemies.add_child(enemy_instances[spawner])
+	# sset position of instance to that of the spawner
+	enemy_instances[spawner].position = spawner.position
+	# hide the spawner
+	spawner.visible = false
+	
+func despawn(spawner):
+	# despawn instance of enemy
+	enemy_instances[spawner].queue_free()
+	# remove dictionary entry
+	enemy_instances.erase(spawner)
+	# shows the spawner
+	spawner.visible = true
+
+
