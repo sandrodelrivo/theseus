67e6899996d7d7da7932b9f003bfa4f2e585ed7a
Sandro <sandro.delrivo@gmail.com>
2018-03-31 10:07:08 -0700

    Player Animations
    Added Player Animations

--- player/player.gd
+++ player/player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 # ---------- variables ----------
 
+# nodes
+
 #  movement
 var speedMax = 100 # upper limit of speedCurrent
 var speedCurrent = 0 # magnitude to which velocity is set
@@ -11,14 +13,17 @@ var dec = 15 # deceleration interval
 var movement = Vector2() # vector which determines movement each physics tick
 
 #  facing
+
 var facing = Vector2()
-var direction = 0
+var direction = 0 # for all directions, 0 - right,  1 - down, 2 - left, 3 - up
+var walk_direction = 0
 
 # attack
 var angleStartRel = -PI/2 # cardinal fcing direction+angleStart
 var angleStopRel = PI/4 # cardinal facing direction+angleStop
 var angleStop = 0
 var swingTime = 15 # number of physics ticks (1/60 seconds)
+
 
 #  stats
 var health = 100
@@ -37,6 +42,7 @@ enum STATE{
 # runs when node is loaded
 func _ready():
 	print("Player Script Ready!")
+	#$anim.play("player_walk_down")
 	# initial state
 	STATE = idle
 
@@ -47,17 +53,54 @@ func _physics_process(delta):
 	facing = get_global_mouse_position()-position
 	# converts angle to int on interval [0,3] with 0rad=0 increasing clockwise
 	direction = range(0,4)[int(round(facing.angle()/(PI/2)))]
-	$sprite.frame = direction*2
-	
-	$camera.position = get_owner().get_node("dash_location").position - position
+
+	#$camera.position = get_owner().get_node("dash_location").position - position
 
 	# performs state processes which need to run each physics tick
 	match STATE:
-		idle:
-			pass
+		idle :
+
+			match walk_direction:
+					0:
+						#print("playing idle right")
+						$anim.play("player_idle_right")
+					1:
+						#print("playing idle down")
+						$anim.play("player_idle_down")
+					2:
+						#print("playing idle left")
+						$anim.play("player_idle_left")
+					3:
+						#print("playing idle up")
+						$anim.play("player_idle_up")
+
+
+
 		move:
+
+			#print("Is Anim Playing? - ", not anim.is_playing())
+
+			match walk_direction:
+					0:
+						if $anim.assigned_animation != "player_walk_right":
+							print("playing walk right")
+							$anim.play("player_walk_right")
+					1:
+						if $anim.assigned_animation != "player_walk_down":
+							print("playing walk down")
+							$anim.play("player_walk_down")
+					2:
+						if $anim.assigned_animation != "player_walk_left":
+							print("playing walk left")
+							$anim.play("player_walk_left")
+					3:
+						if $anim.assigned_animation != "player_walk_up":
+							print("playing walk up")
+							$anim.play("player_walk_up")
+
 			movement = get_velocity()
 			move_and_slide(movement)
+
 		dash:
 			pass
 		melee:
@@ -125,6 +168,8 @@ func set_state(new_state):
 			STATE = idle
 			speedCurrent = 0
 		"move":
+			print("Stopping Anim!")
+			$anim.stop()
 			STATE = move
 		"dash":
 			STATE = dash
@@ -147,6 +192,7 @@ func get_actions():
 		Input.is_action_pressed("ui_left")):
 		event_list["move"] = true
 
+
 	# checks if the right mouse has just been pressed --> dash
 	if (Input.is_action_just_pressed("ui_select")):
 		event_list["dash"] = true
@@ -164,12 +210,16 @@ func get_velocity():
 	# adds 1 to either horizontal or vertical depending on keypress
 	if (Input.is_action_pressed("ui_up")):
 		velocity.y-=1
+		walk_direction = 3
 	if (Input.is_action_pressed("ui_down")):
 		velocity.y+=1
+		walk_direction = 1
 	if (Input.is_action_pressed("ui_right")):
 		velocity.x+=1
+		walk_direction = 0
 	if (Input.is_action_pressed("ui_left")):
 		velocity.x-=1
+		walk_direction = 2
 
 	# increases speed by acc if it is less than speedMax
 	if speedCurrent < speedMax:
